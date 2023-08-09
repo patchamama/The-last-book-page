4b6ed6a4b28eed2c5465776b81189c980413157e
Armando Urquiola Cabrera <patchamama.go@gmail.com>
2023-08-09 14:54:22 +0000

    Add image validation field to dont post large image into profile.image and book.cover serializer

--- books/serializers.py
+++ books/serializers.py
@@ -4,6 +4,19 @@ from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
 
+    def validate_image(self, value):
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        return value
+
     class Meta:
         model = Book
         fields = [
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -6,6 +6,18 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
 
+    def validate_image(self, value):
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
