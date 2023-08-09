fe7398c5f517058bcada6ce5133198cdd4914197
Armando Urquiola Cabrera <patchamama.go@gmail.com>
2023-08-09 23:15:03 +0000

    Add JSON Web Token auth. for the app

--- drf_api_lastpage/settings.py
+++ drf_api_lastpage/settings.py
@@ -24,6 +24,23 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [(
+        'rest_framework.authentication.SessionAuthentication'
+        if 'DEV' in os.environ
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )]
+}
+
+REST_USE_JWT = True
+JWT_AUTH_SECURE = True
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+
+REST_AUTH_SERIALIZERS = {
+    'USER_DETAILS_SERIALIZER': 'drf_api_lastpage.serializers.CurrentUserSerializer'
+}
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -51,6 +68,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
 
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
+
     'profiles',
     'books',
     'comments',
@@ -59,6 +84,7 @@ INSTALLED_APPS = [
     'bookmarks',
 ]
 
+SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- env.py
+++ env.py
@@ -1,2 +1,3 @@
 import os
 os.environ['CLOUDINARY_URL'] = 'cloudinary://533556294858739:VCEH_GE6Lrq01vkbDzFv7fgQn80@dynkvs4t8'
+os.environ['DEV'] = '1'
