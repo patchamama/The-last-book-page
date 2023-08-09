0e6ac51b045cc166ebc7316fdfcdb104e233791c
Armando Urquiola Cabrera <patchamama.go@gmail.com>
2023-08-09 15:58:02 +0000

    Add function to handle duplicate errors in Like serializer

---
+++ likes/serializers.py
@@ -0,0 +1,23 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from likes.models import Like
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Like model
+    The create method handles the unique constraint on 'owner' and 'post'
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Like
+        fields = ['id', 'created_on', 'owner', 'comment']
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
