3e6c0e5a8b33081bc80b0e8b2c16f5a70eef3f1c
Armando Urquiola Cabrera <patchamama.go@gmail.com>
2023-08-10 00:04:19 +0000

    Update setting to deploy in Heroku

--- drf_api_lastpage/settings.py
+++ drf_api_lastpage/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 if os.path.exists('env.py'):
     import env
@@ -39,12 +41,13 @@ if 'DEV' not in os.environ:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
         'rest_framework.renderers.JSONRenderer',
     ]
-    
+
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
-JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_REFRESH_COOKE = 'my-refresh-token'
+JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'drf_api_lastpage.serializers.CurrentUserSerializer'
@@ -55,12 +58,14 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6wb#ip0z2!i%o*pqxqwu4wmwrn)l_y!dnzo_k=#f*@%ttv2e!o'
+# SECRET_KEY = 'django-insecure-6wb#ip0z2!i%o*pqxqwu4wmwrn)l_y!dnzo_k=#f*@%ttv2e!o'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-patchamama-pp5drfapithe-gozrvwzdzbf.ws-eu102.gitpod.io']
+ALLOWED_HOSTS = ['8000-patchamama-pp5drfapithe-gozrvwzdzbf.ws-eu102.gitpod.io', 'localhost', 'drp-api-the-last-book-page.herokuapp.com']
 
 
 # Application definition
@@ -84,6 +89,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
+    'corsheaders',
 
     'profiles',
     'books',
@@ -95,6 +101,7 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -103,6 +110,17 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+if 'CLIENT_ORIGIN' in os.environ:
+     CORS_ALLOWED_ORIGINS = [
+         os.environ.get('CLIENT_ORIGIN')
+     ]
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https://.*\.gitpod\.io$",
+    ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'drf_api_lastpage.urls'
 
@@ -128,12 +146,17 @@ WSGI_APPLICATION = 'drf_api_lastpage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DEV' in os.environ:
+     DATABASES = {
+         'default': {
+             'ENGINE': 'django.db.backends.sqlite3',
+             'NAME': BASE_DIR / 'db.sqlite3',
+         }
+     }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-}
 
 
 # Password validation
--- env.py
+++ env.py
@@ -1,3 +1,6 @@
 import os
+
+os.environ.setdefault("SECRET_KEY", "xrgx655ab&se=c527frx&pw$litc37z+pom@0ct+4c(m&v1yhl")
 os.environ['CLOUDINARY_URL'] = 'cloudinary://533556294858739:VCEH_GE6Lrq01vkbDzFv7fgQn80@dynkvs4t8'
 os.environ['DEV'] = '1'
+os.environ['DATABASE_URL'] = "postgres://vckksofd:481Xg13lQTiF08ckv9G8rXSYXdjC7887@horton.db.elephantsql.com/vckksofd"
