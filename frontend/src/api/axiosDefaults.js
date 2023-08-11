14c770869373264b758ffe53232f2068eeccd837
Armando Urquiola Cabrera <patchamama.go@gmail.com>
2023-08-11 00:45:38 +0000

    add SingUpForm + install axios

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,12 @@
 // IMPORTANT!!
- // Because this React app is running in the same workspace as the API,
+// Because this React app is running in the same workspace as the API,
 
- // there is no need to set a separate baseURL until you reach deployment.
+// there is no need to set a separate baseURL until you reach deployment.
 
- // Setting a baseURL before you reach deployment will cause errors
+// Setting a baseURL before you reach deployment will cause errors
+
+// import axios from "axios";
+
+// axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+// axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+// axios.defaults.withCredentials = true;
