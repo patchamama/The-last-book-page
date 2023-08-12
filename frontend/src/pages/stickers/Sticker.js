a2fb1d2171bcdb51ce5f3e3af4085008f90e18e4
Armando Urquiola Cabrera <patchamama.go@gmail.com>
2023-08-12 14:18:35 +0000

    add option to delete stickers in Sticker component

--- frontend/src/pages/stickers/Sticker.js
+++ frontend/src/pages/stickers/Sticker.js
@@ -3,9 +3,43 @@ import { Media } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import styles from "../../styles/Sticker.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { MoreDropdown } from "../../components/MoreDropdown";
+import { axiosRes } from "../../api/axiosDefaults";
 
 const Sticker = (props) => {
-  const { profile_id, profile_image, owner, updated_on, content } = props;
+  const {
+    profile_id,
+    profile_image,
+    owner,
+    updated_on,
+    content,
+    id,
+    setComment,
+    setStrickers,
+  } = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/stickers/${id}/`);
+      setComment((prevComment) => ({
+        results: [
+          {
+            ...prevComment.results[0],
+            stickers_count: prevComment.results[0].stickers_count - 1,
+          },
+        ],
+      }));
+
+      setStrickers((prevStrickers) => ({
+        ...prevStrickers,
+        results: prevStrickers.results.filter((stricker) => stricker.id !== id),
+      }));
+    } catch (err) {}
+  };
 
   return (
     <div>
@@ -19,6 +53,9 @@ const Sticker = (props) => {
           <span className={styles.Date}>{updated_on}</span>
           <p>Content {content}</p>
         </Media.Body>
+        {is_owner && (
+          <MoreDropdown handleEdit={() => {}} handleDelete={handleDelete} />
+        )}
       </Media>
     </div>
   );
