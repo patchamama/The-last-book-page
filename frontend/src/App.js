bd1084e882a2eae4b3040a5509c7115ce36f5272
Armando Urquiola Cabrera <patchamama.go@gmail.com>
2023-08-12 17:21:04 +0000

    add option to edit profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,9 @@ import CommentsPage from "./pages/comments/CommentsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 import CommentEditForm from "./pages/comments/CommentEditForm";
 import ProfilePage from "./pages/profiles/ProfilePage";
+import UsernameForm from "./pages/profiles/UsernameForm";
+import UserPasswordForm from "./pages/profiles/UserPasswordForm";
+import ProfileEditForm from "./pages/profiles/ProfileEditForm";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -60,6 +63,21 @@ function App() {
             render={() => <CommentEditForm />}
           />
           <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
+          <Route
+            exact
+            path="/profiles/:id/edit/username"
+            render={() => <UsernameForm />}
+          />
+          <Route
+            exact
+            path="/profiles/:id/edit/password"
+            render={() => <UserPasswordForm />}
+          />
+          <Route
+            exact
+            path="/profiles/:id/edit"
+            render={() => <ProfileEditForm />}
+          />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
